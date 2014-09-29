1ceb5723ba984f50963d4a33596fffd026cc0366
maxsteel25ft <maxsteel25ft@yahoo.es>
2014-09-28 21:46:47 -0400

    Subida numero 4
    Mejorando un poco las explicaciones.

--- app/src/main/java/com/example/julio/asynctask/MyActivity.java
+++ app/src/main/java/com/example/julio/asynctask/MyActivity.java
@@ -14,85 +14,82 @@ import android.widget.TextView;
 /*
 *	Modificado de un original obtenido desde la Internet
 *	Septiembre 28, 2014
-*	New York
+*	New York, USA.
 */
-
 
 public class MyActivity extends Activity implements OnClickListener {
 	int contador = 0;
-		Button btn;
+	Button btn;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my);
+		btn = (Button) findViewById(R.id.boton);
+		btn.setOnClickListener(this);
+
+	}
+
+	public void onClick(View view) {
+		// detect the view that was "clicked"
+		switch (view.getId()) {
+			case R.id.boton:
+				new LongOperation().execute("");
+				contador++;
+				break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class LongOperation extends AsyncTask<String, Void, String> {
 
 		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_my);
-			btn = (Button) findViewById(R.id.boton);
-			// because we implement OnClickListener we only have to pass "this"
-			// (much easier)
-			btn.setOnClickListener(this);
-
-		}
-
-		public void onClick(View view) {
-			// detect the view that was "clicked"
-
-			switch (view.getId()) {
-				case R.id.boton:
-					new LongOperation().execute("");
-					contador ++;
-					break;
-			}
-		}
-
-		private class LongOperation extends AsyncTask<String, Void, String> {
-
-			@Override
-			protected String doInBackground(String... params) {
-				for (int i = 0; i < 5; i++) {
-					try {
-						Thread.sleep(50);
-					} catch (InterruptedException e) {
-						Thread.interrupted();
-
-					}
-					String cuenta = Integer.toString(i);
-					Log.d("***TRY",cuenta);
+		protected String doInBackground(String... params) {
+			for (int i = 0; i < 5; i++) {
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					Thread.interrupted();
 				}
-				return "Executed";
+				String cuenta = Integer.toString(i);
+				Log.d("***TRY", cuenta);
 			}
-
-			@Override
-			protected void onPostExecute(String result) {
-				TextView txt = (TextView) findViewById(R.id.output);
-				String cuenta = Integer.toString(contador);
-				txt.setText("*** " + cuenta + " ***"); // txt.setText(result);
-				// might want to change "executed" for the returned string passed
-				// into onPostExecute() but that is upto you
-			}
-
-			@Override
-			protected void onPreExecute() {}
-
-			@Override
-			protected void onProgressUpdate(Void... values) {}
+			return "Finalizado ...";
 		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+		@Override
+		protected void onPostExecute(String result) {
+			TextView txt = (TextView) findViewById(R.id.output);
+			String cuenta = Integer.toString(contador);
+			txt.setText("*** " + cuenta + " ***"); // txt.setText(result);
+			// might want to change "executed" for the returned string passed
+			// into onPostExecute() but that is upto you
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+		}
+	}
 }
